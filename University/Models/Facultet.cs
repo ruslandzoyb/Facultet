ae0b9e536b0b8fd3204c847792453b4cdd170662
Dzobko Ruslan <Ruslan_Dzobko@epam.com>
2020-05-30 16:12:24 +0300

    Добавьте файлы проекта.

---
+++ University/Models/Facultet.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace University.Models
+{
+   public class Facultet
+    {
+    }
+}
---
+++ University/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace University.Models
+{
+  public  class Student
+    {
+        public int MyProperty { get; set; }
+    }
+}

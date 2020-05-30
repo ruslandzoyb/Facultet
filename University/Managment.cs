1af3f114d981e02f0762a71ef9628de33330f34d
Dzobko Ruslan <Ruslan_Dzobko@epam.com>
2020-05-30 16:20:49 +0300

    Add management

---
+++ University/Managment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+using University.Models;
+
+namespace University
+{
+  public static  class Managment
+    {
+
+        public static Facultet CreateFacultet (string name)
+        {
+            return new Facultet() { Name = name };
+        } 
+    }
+}
